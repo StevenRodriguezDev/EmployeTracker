48ca7ae529d6fb7e50fa70ce1be15eea11db9b5f
StevenRodriguezDev <stevenrodriguez3913@gmail.com>
2023-10-04 22:34:33 -0600

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,24 +5,29 @@ USE employee_db;
 
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
-  department_id INT,
-  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE;
+  department_id INT NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY (department_id) 
+  REFERENCES department(id) ON DELETE CASCADE;
 );
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  salary DECIMAL,
-  role_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  role_id INT NOT NULL,
   manager_id INT,
-  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE;
+  PRIMARY KEY(id),
+  FOREIGN KEY (role_id) 
+  REFERENCES role(id) ON DELETE CASCADE,
+  FOREIGN KEY (manager_id) 
+  REFERENCES employee(id) ON DELETE CASCADE;
 );
