c6e2bc59e9add760709ea226f1458ca7efb73874
StevenRodriguezDev <stevenrodriguez3913@gmail.com>
2023-10-04 22:34:41 -0600

    updated seeds

--- db/seed.sql
+++ db/seed.sql
@@ -1,21 +1,29 @@
 INSERT INTO department (name)
-VALUES ("Web Development"),
-       ("Data Science"),
-       ("Math"),
-       ("Electives");
+VALUES ("Sales Lead"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Intro to JavaScript", 50000.00, 1),
-       ("Data Science",100000.00, 2),
-       ("Linear Algebra",105000.00, 3),
-       ("History of the Internet",110000.00, 4),
-       ("Machine Learning",200000.00, 4),
-       ("Game Design", 150000.00, 1 ),
-       ("Cloud Development",160000.00, 1);
+VALUES ("Sales Lead", 100000, 1),
+("Salesperson", 80000. 1),
+("Lead Engineer", 150000, 2),
+("Software Engineer", 120000, 2),
+("Account Manager", 160000, 3),
+("Accountant", 125000, 3),
+("Legal Team Lead", 250000, 4),
+("Lawyer", 190000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", null, 1),
-       ("Paul","Smith", null, null);
+VALUES ("John", "Doe", 1, null),
+("Mike", "Chan", 2, 1),
+("Ashley", "Rodriguez", 3, null),
+("Kevin", "Tupik", 4, 3),
+("Kunal", "Singh", 5, null),
+("Malia", "Brown", 6, 5),
+("Sarah". "Lourd", 7, null),
+("Tom", "Allen", 8, 7),
+
 
 
        
