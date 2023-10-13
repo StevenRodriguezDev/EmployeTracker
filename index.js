120034abf1ecc80e807173f5f8adb44c3a1e22f6
StevenRodriguezDev <stevenrodriguez3913@gmail.com>
2023-10-13 00:01:06 -0600

    updated index.js

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ const initialQuestion = [
 
 const init = async () => {
   const { initialUserAction } = await inquirer.prompt(initialQuestion);
+  console.log(initialUserAction);
   switch (initialUserAction) {
     case "view departments":
       viewDepartment();
@@ -71,13 +72,18 @@ const viewDepartment = async () => {
   console.table(result);
   init();
 };
+
+
 const viewAllRole = async () => {
   const roleResult = await query("SELECT * FROM role");
   console.table(roleResult);
   init();
 };
+
+
 const viewAllEmployees = async () => {
-  const employeeResult = await query(`SELECT e.id, CONCAT(e.first_name, " ", e.last_name) AS name, r.title, d.name AS department, CONCAT(e2.first_name, " ", e2.last_name) AS manager, r.salary
+  const employeeResult =
+    await query(`SELECT e.id, CONCAT(e.first_name, " ", e.last_name) AS name, r.title, d.name AS department, CONCAT(e2.first_name, " ", e2.last_name) AS manager, r.salary
   FROM employee AS e
   JOIN role AS r 
   ON e.role_id = r.id
@@ -91,7 +97,6 @@ const viewAllEmployees = async () => {
   init();
 };
 
-
 const addDepartment = async () => {
   const addDepartmentQuestion = [
     {
@@ -105,31 +110,127 @@ const addDepartment = async () => {
   viewDepartment();
 };
 
-
-const addDepartment = async () => {
-  const addDepartmentQuestion = [
+const addRole = async () => {
+  const departments = await query("SELECT id AS value, name FROM department");
+  const addRoleQuestion = [
     {
       type: "input",
-      message: "Type in the name of the new department.",
-      name: "name",
+      message: "Input the role name.",
+      name: "title",
     },
-  ];
-  const { name } = await inquirer.prompt(addDepartmentQuestion);
-  await query("INSERT INTO department (name) VALUES(?)", [name]);
-  viewDepartment();
-};
-
-
-const addDepartment = async () => {
-  const addDepartmentQuestion = [
     {
       type: "input",
-      message: "Type in the name of the new department.",
-      name: "name",
+      message: "Input the salary amount.",
+      name: "salary",
+    },
+    {
+      type: "list",
+      message: "Select the department this role is within.",
+      name: "department_id",
+      choices: departments,
     },
   ];
-  const { name } = await inquirer.prompt(addDepartmentQuestion);
-  await query("INSERT INTO department (name) VALUES(?)", [name]);
-  viewDepartment();
+  const { title, salary, department_id } = await inquirer.prompt(
+    addRoleQuestion
+  );
+  await query("INSERT INTO role (title, salary, department_id) VALUES(?,?,?)", [
+    title,
+    parseInt(salary),
+    department_id,
+  ]);
+  viewAllRole();
 };
 
+addEmployee = async () => {
+  const roles = await query("SELECT title AS name, id AS value FROM role");
+  const managers = await query(
+    "SELECT CONCAT(first_name, ' ', last_name) as name, id AS value FROM employee WHERE manager_id IS null"
+  );
+  managers.push({
+    name: "No Manager",
+    value: null,
+  });
+  console.log(roles);
+  const addEmployeeQuestions = [
+    {
+      type: "input",
+      message: "Input the employee's first name.",
+      name: "first_name",
+    },
+    {
+      type: "input",
+      message: "Input the employee's last name.",
+      name: "last_name",
+    },
+    {
+      type: "list",
+      message: "Select the title this employee holds.",
+      name: "role_id",
+      choices: roles,
+    },
+    {
+      type: "list",
+      message: "Select this employee's manager.",
+      name: "manager_id",
+      choices: managers,
+    },
+  ];
+  const { first_name, last_name, role_id, manager_id } = await inquirer.prompt(
+    addEmployeeQuestions
+  );
+  await query(
+    "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES(?, ?, ?, ?)",
+    [first_name, last_name, role_id, manager_id]
+  );
+  viewEmployees();
+};
+
+updateEmployee = async () => {
+  const employeeUpdateOptions = await query(
+    "SELECT CONCAT(first_name, ' ', last_name) as name, id AS value FROM employee"
+  );
+  const roles = await query("SELECT title AS name, id AS value FROM role");
+  const managers = await query(
+    "SELECT CONCAT(first_name, ' ', last_name) as name, id AS value FROM employee WHERE manager_id IS null"
+  );
+  managers.push({
+    name: "No Manager",
+    value: null,
+  });
+  const updateEmployeeQuestions = [
+    {
+      type: "list",
+      message: "Select the employee you would like to work with.",
+      name: "id",
+      choices: employeeUpdateOptions,
+    },
+    {
+      type: "list",
+      message: "Select the role this employee now holds.",
+      name: "role_id",
+      choices: roles,
+    },
+    {
+      type: "list",
+      message: "Select the manager this employee will be under, if applicable.",
+      name: "manager_id",
+      choices: managers,
+    },
+  ];
+  const { id, role_id, manager_id } = await inquirer.prompt(
+    updateEmployeeQuestions
+  );
+  await query("UPDATE employee SET role_id =?, manager_id =? WHERE id=?", [
+    role_id,
+    manager_id,
+    id,
+  ]);
+  viewEmployees();
+};
+
+const quit = () => {
+  console.log("Goodbye.");
+  process.exit();
+};
+
+init();
